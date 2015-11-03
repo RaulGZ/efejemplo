1078027e22f99c4048ff629b493cfff7c9b914fb
Raul Gallegos <Chino_rgz@hotmail.com>
2015-11-03 08:47:36 -0800

    primer commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Ejercicio01.mibd;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace Ejercicio01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (Regex.IsMatch(txtNombre.Text, @"^[a-zA-Z]+$"))
+                {
+                if (Regex.IsMatch(txtSueldo.Text, @"\d+$"))
+                {
+                    //1.- Instanciar la "Base de Datos"
+                    demoEF db = new demoEF();
+                    //2.- Instanciar "Tabla empleados"
+                    Empleado emp = new Empleado();
+                    emp.Nombre = txtNombre.Text;
+                    emp.Sueldo = int.Parse(txtSueldo.Text);
+                    //agregar los datos capturados
+                    db.Empleados.Add(emp);
+                    db.SaveChanges();
+                }
+                else { MessageBox.Show("Solo numeros #sueldo"); }
+                }
+            else { MessageBox.Show("Solo letras #Nombre"); }   
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (Regex.IsMatch(txtNombre.Text, @"^[a-zA-Z]+$"))
+                {
+                    if (Regex.IsMatch(txtid.Text, @"\d+$") && Regex.IsMatch(txtSueldo.Text, @"\d+$"))
+                {
+                //1.- Instanciar "Base de Datos"
+                demoEF db = new demoEF();
+                //2.- Buscar el id capturado en la caja de texto
+                int id = int.Parse(txtid.Text);
+                //var es una variable dinamica
+                var emp = db.Empleados.SingleOrDefault(x => x.id == id);
+                if (emp != null) {
+                    //asignar los nuevos valores
+                    emp.Nombre = txtNombre.Text;
+                    emp.Sueldo = int.Parse(txtSueldo.Text);
+                    db.SaveChanges();
+                }
+                }
+                else { MessageBox.Show("Verifique que solo sean numeros en #id y #sueldo"); }
+                }
+            else { MessageBox.Show("Solo letras #Nombre"); }   
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (Regex.IsMatch(txtid.Text, @"\d+$"))
+            {
+                demoEF db = new demoEF();
+                //Buscar el id capturado en la caja de texto
+                int id = int.Parse(txtid.Text);
+                var emp = db.Empleados.SingleOrDefault(x => x.id == id);
+                if (emp != null)
+                {
+                    //eliminar el registros
+                    db.Empleados.Remove(emp);
+                    db.SaveChanges();
+                }
+            }
+            else { MessageBox.Show("Solo numeros #id"); }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            if (Regex.IsMatch(txtid.Text, @"\d+$"))
+                {
+            //Consultar solo por ID
+            demoEF db = new demoEF();
+            int id = int.Parse(txtid.Text);
+            var registros = from s in db.Empleados
+                            where s.id == id
+                            select new
+                            {
+                                s.Nombre,
+                                s.Sueldo
+                            };
+            dbgrid.ItemsSource = registros.ToList();
+                }
+            else { MessageBox.Show("Solo numeros #id"); }
+
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            //Consultar solo por ID
+            demoEF db = new demoEF();
+            var registros = from s in db.Empleados
+                            select s;
+            dbgrid.ItemsSource = registros.ToList();
+        }
+    }
+}
---
+++ mibd/demoEF.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01.mibd
+{
+    public class demoEF : DbContext
+    {
+        public DbSet<Empleado> Empleados { get; set; }
+        //para crear la base de datos, DbSet es la palabra reservada
+    }
+}
